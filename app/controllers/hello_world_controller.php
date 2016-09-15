ca8fdb7d2a2a86359885c6d70d1bee3ed5bba4f3
Lataamo <lataamo@lataamo-VirtualBox.(none)>
2016-09-15 22:51:00 +0300

    Ma

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,38 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+     // echo 'Hello World!';
+     // echo ' I made this!';
+        View::make('helloworld.html');
+    }
+    
+    public static function Esittely(){
+        View::make('suunnitelmat/Esittely.html');
+    }
+    
+    public static function Hakusivu() {
+        View::make('suunnitelmat/Hakusivu.html');
+    }
+    
+    public static function Hakutulossivu() {
+        View::make('suunnitelmat/Hakutulossivu.html');
+    }
+    
+    public static function Kirjautumissivu() {
+        View::make('suunnitelmat/Kirjautumissivu.html');
+    }
+    
+    public static function Muokkaussivu() {
+        View::make('suunnitelmat/Muokkaussivu.html');
+    }
+    
+    public static function Rekisteroitymissivu() {
+        View::make('suunnitelmat/Rekisteroitymissivu.html');
     }
   }
+  
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,27 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/Esittely', function() {
+    HelloWorldController::Esittely();
+  });
+  
+  $routes->get('/Hakusivu', function() {
+    HelloWorldController::Hakusivu();
+  });
+  
+  $routes->get('/Hakutulossivu', function() {
+    HelloWorldController::Hakutulossivu();
+  });
+  
+  $routes->get('/Kirjautumissivu', function() {
+    HelloWorldController::Kirjautumissivu();
+  });
+  
+  $routes->get('/Muokkaussivu', function() {
+    HelloWorldController::Muokkaussivu();
+  });
+  
+  $routes->get('/Rekisteroitymissivu', function() {
+    HelloWorldController::Rekisteroitymissivu();
+  });
