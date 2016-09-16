d4e9c6013501b481c5171cc36ff26996163dc8d8
Lataamo <lataamo@lataamo-VirtualBox.(none)>
2016-09-16 05:32:04 +0300

    Tauluja

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,10 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (nimi, salasana) VALUES ('Pena', 'abc33')
+INSERT INTO Kayttaja (nimi, salasana) VALUES ('Sanna', 'password')
+
+INSERT INTO Yllapitaja (nimi, salasana) VALUES ('Timppa', '7ye7')
+
+INSERT INTO Sairaus (nimi, lisayspvm) VALUES ('Lorvikatarri', '0909')
+
+INSERT INTO Geeni (nimi, lisayspvm) VALUES ('SHH', '12122014')
+
+INSERT INTO Mutaatio (sijainti, tyyppi) VALUES ('A313K', 'Harmiton')
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,38 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+id SERIAL PRIMARY KEY,
+nimi varchar(50) NOT NULL,
+salasana varchar(50) NOT NULL,
+email varchar(50) NOT NULL
+);
+
+CREATE TABLE Yllapitaja(
+id SERIAL PRIMARY KEY,
+nimi varchar(50) NOT NULL,
+salasana varchar(50) NOT NULL,
+email varchar(50) NOT NULL
+);
+
+CREATE TABLE Sairaus(
+id SERIAL PRIMARY KEY,
+nimi varchar(50) NOT NULL,
+geenit text,
+mutaatiot text,
+lisayspvm date NOT NULL
+);
+
+CREATE TABLE Geeni(
+id SERIAL PRIMARY KEY,
+nimi varchar(50) NOT NULL,
+mutaatiot text,
+sairaudet text,
+lisayspvm date NOT NULL
+);
+
+CREATE TABLE Mutaatio(
+id SERIAL PRIMARY KEY,
+sijainti varchar(50) NOT NULL,
+tyyppi varchar(50) NOT NULL,
+lisayspvm date NOT NULL,
+geeni varchar(50) REFERENCES Geeni(nimi) NOT NULL,
+sairaus varchar(50) REFERENCES Sairaus(nimi) NOT NULL
+);
